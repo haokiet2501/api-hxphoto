0bfa584cf49e2c5f6bd8a9ab91c0fb8caa5fd30a
Kiệt Lê <lekietpro123@gmail.com>
2023-06-04 11:27:50 +0700

    Update data

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const PORT = 4000
 
 server.use(middlewares)
 // Add this before server.use(router)
@@ -11,7 +12,7 @@ server.use(jsonServer.rewriter({
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
-server.listen(3000, () => {
+server.listen(4000, () => {
     console.log('JSON Server is running')
 })
 
